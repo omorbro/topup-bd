c77b2763646bf3f5d0e826a85557084afa84e20d
omorbro <jilanibro25@gmail.com>
2026-07-11 19:00:57 +0600

    Add Firebase config and admin page

---
+++ lib/firebase.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDE7J_k7d8RJB39LmjQXGuFWm7AmpjAWI",
+  authDomain: "topup-bd-600b5.firebaseapp.com",
+  projectId: "topup-bd-600b5",
+  storageBucket: "topup-bd-600b5.firebasestorage.app",
+  messagingSenderId: "950554800995",
+  appId: "1:950554800995:web:e45d692acf00d75ebb5956",
+  measurementId: "G-D6399XFRDE"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export default app;
